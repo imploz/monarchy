306c1df9332b6ea215d0c5ccc7dc8852c9715d17
szymon <szy.m@n>
2019-03-07 14:43:02 +0100

    elementary json parser (no error handling)

---
+++ monarchy/json_parser.py
@@ -0,0 +1,6 @@
+import json
+
+
+def parse(json_content: str):
+    return json.loads(json_content)
+
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 from .context import monarchy
 from monarchy import downloader
 from monarchy import config
+from monarchy import json_parser
 
 
 # WARNING: it's an integration text testing whole functionality, including downloading a json file from the internet
@@ -8,4 +9,7 @@ from monarchy import config
 
 def test_get_monarchs():
     r = downloader.get(config.MONARCHS_URL)
-    assert len(r) > 10
+    monarchs = json_parser.parse(r)
+
+    assert len(monarchs) > 10
+
